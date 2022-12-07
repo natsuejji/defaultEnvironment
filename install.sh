8d3bc894c069cc5d41fbe1273fdef1aa54d21682
lyh <natsuejji1@gmail.com>
2022-12-07 17:42:21 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,33 @@
-!/bin/bash
 # install curl
-
 sudo apt install curl
 
 # install zsh and p9k
+
 sudo apt install zsh
 
 # install oh my zsh
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# exit
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# q
+
 cp zshrc.example ~/.zshrc
-cp p9k.zsh.example .p10k.zsh
+
+cp p10k.zsh.example ~/.p10k.zsh
+
+zsh ~/.p10k.zsh
+
+chsh -s $(which zsh);
+
+
 
 # install nvm (for markdown preview)
+
+
 
 export NVM_DIR="$HOME/.nvm" && (
 
@@ -23,21 +38,37 @@ export NVM_DIR="$HOME/.nvm" && (
        git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 
 ) && \. "$NVM_DIR/nvm.sh"
-source ~/.zshrc
+
+source ~/.bashrc
+
 nvm install --lts
 
+
+
 # install markdown preview tool
+
 sudo apt install xdg-utils
-nvm -g install instant-markdown-d
+
+node -g install instant-markdown-d
+
+
 
 # install tmux
+
 sudo apt install tmux
+
 cp tmux.conf.example ~/.tmux.conf
 
 # install vim
 
 sudo apt install vim
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 cp vimrc.example ~/.vimrc
 
+chsh -s $(which zsh);
+
+echo 'please reboot your pc.';
