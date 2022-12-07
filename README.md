c7c7ba4a3d9072bdac5c7bccc5ffffd73805f7c6
lyh <natsuejji1@gmail.com>
2022-12-07 18:15:22 +0800

    modify install.sh and readme

--- README.md
+++ README.md
@@ -5,15 +5,34 @@ ubuntu/linux develop environment setting
 ---
 
 ##  prerequisite
+
 * font
+
 ```
+
 https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
-```
-___
 
-## install
+```
+
+* nvm and nodejs
+# install nvm (for markdown preview)
 ```shell
-sh install.sh
+
+export NVM_DIR="$HOME/.nvm" && (
+   git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+     cd "$NVM_DIR"
+       git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+source ~/.zshrc
+
+nvm install --lts
+
 ```
+## install
 
+```shell
 
+sh install.sh
+
+```
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # install curl
+
 sudo apt install curl
 
 # install zsh and p9k
@@ -9,66 +10,22 @@ sudo apt install zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# exit
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# q
-
 cp zshrc.example ~/.zshrc
-
 cp p10k.zsh.example ~/.p10k.zsh
-
-zsh ~/.p10k.zsh
-
-chsh -s $(which zsh);
-
-
-
-# install nvm (for markdown preview)
-
-
-
-export NVM_DIR="$HOME/.nvm" && (
-
-   git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-
-     cd "$NVM_DIR"
-
-       git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-
-) && \. "$NVM_DIR/nvm.sh"
-
-source ~/.bashrc
-
-nvm install --lts
-
-
-
+sh ~/.p10k.zsh
 # install markdown preview tool
-
 sudo apt install xdg-utils
-
 node -g install instant-markdown-d
-
-
-
 # install tmux
-
 sudo apt install tmux
-
 cp tmux.conf.example ~/.tmux.conf
-
 # install vim
-
 sudo apt install vim
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-
-	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp vimrc.example ~/.vimrc
 
 chsh -s $(which zsh);
-
 echo 'please reboot your pc.';
