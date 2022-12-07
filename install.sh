d9bbba5cafc252b6a592431f0aac8097da7019da
lyh <natsuejji1@gmail.com>
2022-12-07 16:46:14 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt install zsh
 
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=0 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 cp zshrc.example ~/.zshrc
 cp p9k.zsh.example .p10k.zsh
 
